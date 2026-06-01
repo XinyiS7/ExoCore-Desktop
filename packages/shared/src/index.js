513cf993367a871cd910acf023283094a29f5b15
AliciaT <aliciaju@126.com>
2026-06-01 16:09:22 +0200

    chore: scaffold shared package

---
+++ packages/shared/src/index.js
@@ -0,0 +1,3 @@
+export { baseUrl, getCsrfToken, MODEL_REGISTRY, AVAILABLE_MODELS, getConvProjectId } from './api';
+export { useApi } from './hooks/useApi';
+export { useCsrf } from './hooks/useCsrf';
